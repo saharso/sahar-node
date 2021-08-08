f540bcbba981cd9752588334f4c60a143c2dc7b7
Sahar Soffer <ssoffer@medallia.com>
2021-08-08 11:49:56 +0300

    move iframe appending to dynamic element function to SandBoxIframe static method

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-
+const courses = [
+    {id: 1, name: 'course 1'},
+    {id: 2, name: 'course 2'},
+    {id: 3, name: 'course 3'},
+]
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4, 'haudy dooo']);
+    res.send(courses);
+});
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(e => e.id === +req.params.id);
+    if(! course) res.status(404).send(`Course ${req.params.id} not found`);
+    res.send(course);
 });
 
 const port = process.env.PORT || 1010;
